a012a05e41034b080a1e06c616fcc07637210f90
rayhanfdly <126652181+rayhanfdly@users.noreply.github.com>
2023-03-01 14:22:09 +0700

    Add files via upload

---
+++ 0651120096_MuhamadRayhanFadly_mopro.dart
@@ -0,0 +1,80 @@
+//Simple Class Dart
+class Bicycle {
+  int cadence;
+  int _speed = 0;
+  int get speed => _speed;
+  int gear;
+
+  Bicycle(this.cadence, this.gear);
+
+  void applyBrake(int decrement) {
+    _speed -= decrement;
+  }
+
+  void speedUp(int increment) {
+    _speed += increment;
+  }
+
+  @override
+  String toString() => 'Bicycle: $_speed mph';
+}
+
+void main() {
+  var bike = Bicycle(2, 1);
+  print(bike);
+}
+
+
+
+//Factory
+import 'dart:math';
+
+abstract class Shape {
+  num get area;
+}
+
+class Circle implements Shape {
+  final num radius;
+  Circle(this.radius);
+  num get area => pi * pow(radius, 2);
+}
+
+class Square implements Shape {
+  final num side;
+  Square(this.side);
+  num get area => pow(side, 2);
+}
+
+main() {
+  final circle = Circle(2);
+  final square = Square(2);
+  print(circle.area);
+  print(square.area);
+}
+
+//instead of overloading
+main() {
+  print(Rectangle(origin: const Point(10, 20), width: 100, height: 200));
+  print(Rectangle(origin: const Point(10, 10)));
+  print(Rectangle(width: 200));
+  print(Rectangle());
+}
+
+
+
+
+//Implement on interface
+class CircleMock implements Circle {
+  num area = 0;
+  num radius = 0;
+}
+
+//functional programming
+String scream(int length) => "A${'a' * length}h!";
+
+main() {
+  final values = [1, 2, 3, 5, 10, 50];
+  for (var length in values) {
+    print(scream(length));
+  }
+}
